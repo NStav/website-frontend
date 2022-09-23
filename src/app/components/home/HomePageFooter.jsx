47a69b00b34e34ba0efe88fc7f0ccf664b0aadf2
ChangruiHu <huchangrui91@gmail.com>
2022-09-23 18:45:45 +1000

    fixed footer

--- src/app/components/home/HomePageFooter.jsx
+++ src/app/components/home/HomePageFooter.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import "./HomeStyles.css";
 
-import Logo from "./img/Logo.png"
-import LinkedinLogo from "./img/LinkedinLogo.png"
-import instagramLogo from "./img/instagramLogo.png"
-import twitterLogo from "./img/twitterLogo.png"
+import Logo from "./img/Logo.png";
+import LinkedinLogo from "./img/LinkedinLogo.png";
+import instagramLogo from "./img/instagramLogo.png";
+import twitterLogo from "./img/twitterLogo.png";
 
-export default function HomePageFooter(){
-    return(
-        <footer className="footer-container">
-            <div className="footer-grid">
-                <img src={Logo} alt="Redback Logo" className="footer-logo"/>
-
-                <div className="socials-logo"><img src={LinkedinLogo} alt="Redback LinkedIn" /> <p>LinkedIn</p></div>
-                <div className="socials-logo"><img src={instagramLogo} alt="Redback Instagram" /> <p>Instagram</p></div>
-                <div className="socials-logo"><img src={twitterLogo} alt="Redback Twitter" /> <p>Twitter</p></div>
-            </div>
-            <p className="footer-msg">Join us today and enjoy a fully immersive experience where you will enjoy individual exercise routines all the time while receiving real-time feedback and tips on how to improve their performance. Share your exercise with our community and engage with other users both casually and competitively. 
-               <br /><br /> Act now and be the best person you can be.
-            </p>
-        </footer>
-    )
+export default function HomePageFooter() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-grid">
+        <div className="socials-logo lkd">
+          <img src={LinkedinLogo} alt="Redback LinkedIn" />
+          <p>LinkedIn</p>
+        </div>
+        <div className="socials-logo ig">
+          <img src={instagramLogo} alt="Redback Instagram" />
+          <p>Instagram</p>
+        </div>
+        <div className="socials-logo twt">
+          <img src={twitterLogo} alt="Redback Twitter" />
+          <p>Twitter</p>
+        </div>
+      </div>
+      <p className="footer-msg">
+        Join us today and enjoy a fully immersive experience where you will
+        enjoy individual exercise routines all the time while receiving
+        real-time feedback and tips on how to improve their performance. Share
+        your exercise with our community and engage with other users both
+        casually and competitively. Act now and be the best person you can be.
+      </p>
+    </footer>
+  );
 }
--- src/app/components/login/LogInPageFooter.jsx
+++ src/app/components/login/LogInPageFooter.jsx
@@ -10,16 +10,17 @@ export default function LogInPageFooter() {
   return (
     <footer className="footer-container">
       <div className="footer-grid">
-        <img src={Logo} alt="Redback Logo" className="footer-logo" />
-
-        <div className="socials-logo">
-          <img src={LinkedinLogo} alt="Redback LinkedIn" /> <p>LinkedIn</p>
+        <div className="socials-logo lkd">
+          <img src={LinkedinLogo} alt="Redback LinkedIn" />
+          <p>LinkedIn</p>
         </div>
-        <div className="socials-logo">
-          <img src={instagramLogo} alt="Redback Instagram" /> <p>Instagram</p>
+        <div className="socials-logo ig">
+          <img src={instagramLogo} alt="Redback Instagram" />
+          <p>Instagram</p>
         </div>
-        <div className="socials-logo">
-          <img src={twitterLogo} alt="Redback Twitter" /> <p>Twitter</p>
+        <div className="socials-logo twt">
+          <img src={twitterLogo} alt="Redback Twitter" />
+          <p>Twitter</p>
         </div>
       </div>
       <p className="footer-msg">
@@ -27,9 +28,7 @@ export default function LogInPageFooter() {
         enjoy individual exercise routines all the time while receiving
         real-time feedback and tips on how to improve their performance. Share
         your exercise with our community and engage with other users both
-        casually and competitively.
-        <br />
-        <br /> Act now and be the best person you can be.
+        casually and competitively. Act now and be the best person you can be.
       </p>
     </footer>
   );
