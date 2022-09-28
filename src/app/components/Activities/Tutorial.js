aa08b7cbc68223bff25d6da2448aea1d6d34b388
LamKeee <namnh14162000@gmail.com>
2022-09-29 01:30:56 +1000

    m

---
+++ src/app/components/Activities/Tutorial.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/styles';
+
+
+
+const useStyles = makeStyles(() => ({
+    paper: {
+      background: '#5A226D',
+  
+    },
+    paper1: {
+      marginTop: '82px',
+      marginRight: '',
+      marginLeft: '',
+      height: '280px',
+      width: '280px',
+  
+  
+    },
+  
+  }))
+
+export const Tutorial = () => {
+    return (
+        <div> 
+            <Typography> kasdfjsad</Typography> 
+            </div> 
+
+
+
+
+    )
+    
+
+
+
+
+
+} 
+
+
+export default Tutorial; 
+
+
+
