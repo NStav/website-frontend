08e42c9ce899f49828ada7ad9642967652bfd257
NStav <101630268+NStav@users.noreply.github.com>
2023-08-31 12:37:20 +1000

    Add files via upload

--- src/app/components/community/CommunityComponent.jsx
+++ src/app/components/community/CommunityComponent.jsx
@@ -24,22 +24,25 @@ export default function CommunityComponent() {
           Welcome......
         </p>
         <div className="Buttons1">
-        <Button
-              style={{
-                backgroundColor: "black",
-                padding: "18px 36px",
-                fontSize: 35,
-                fontWeight: 900,
-              }}
-              variant="contained"
-              onClick={handleCloseNavMenu}
-              exact
-              component={NavLink}
-              to="/community"
-            >
-              Channels
+
+          <div className="Buttons1Spacing"> 
+            <Button
+                style={{
+                  backgroundColor: "black",
+                  padding: "18px 36px",
+                  fontSize: 35,
+                  fontWeight: 900,
+                }}
+                variant="contained"
+                onClick={handleCloseNavMenu}
+                exact
+                component={NavLink}
+                to="/community"
+              >
+                Channels
             </Button>
-              
+          </div>
+          <div className="Buttons1Spacing">
             <Button
               style={{
                 backgroundColor: "black",
@@ -56,8 +59,9 @@ export default function CommunityComponent() {
             >
               Messages 
             </Button>
-              
-            <Button
+          </div>
+          <div>
+          <Button
               style={{
                 
                 backgroundColor: "black",
@@ -74,9 +78,12 @@ export default function CommunityComponent() {
             >
               Members
             </Button>
+          </div>
         </div>
+
         <div className="Buttons2">
-        <Button
+          <div className="Buttons2Spacing">
+            <Button
               style={{
                 backgroundColor: "#E87461",
                 padding: "18px 36px",
@@ -92,7 +99,8 @@ export default function CommunityComponent() {
             >
               Your Daily Summary
             </Button>
-              
+          </div>
+          <div className="Button2Spacing2">
             <Button
               style={{
                 
@@ -111,7 +119,9 @@ export default function CommunityComponent() {
             >
               The Exercise Guide
             </Button>
+          </div>
         </div>
+
         <div className="Buttons3">
         <Button
               style={{
@@ -134,35 +144,15 @@ export default function CommunityComponent() {
               <h2> Our Platform</h2>
               <h3>We are the world leading platform that provides users:</h3>
               <div className="StatsGrid">
-                  <div className="Column1"> 
-                  4566
-                  </div>
-                  <div className="Column2"> 
-                  35 hrs
-                  </div>
-                  <div className="Column3"> 
-                  36
-                  </div>
-                  <div className="Column4"> 
-                  12
-                  </div>
-                  <div className="Row2"> 
-                  Daily Active Users
-                  </div>
-                  <div className="Row2"> 
-                  Average Time Spent
-                  </div>
-                  <div className="Row2"> 
-                  Scheduled Classes
-                  </div>
-                  <div className="Row2"> 
-                  Ranking
-                  </div>
+                  <div className="StatsGridObject"> <h1>4556</h1><h2>Daily Active Users</h2></div>
+                  <div className="StatsGridObject"> <h1>35 hrs</h1><h2>Average Time Spent</h2></div>
+                  <div className="StatsGridObject"> <h1>36</h1><h2>Scheduled Classes</h2></div>
+                  <div className="StatsGridObject"> <h1>12</h1><h2>Ranking</h2></div>
               </div>
       </div>
       <div className="CommunityContent2">
         <h1>Quick Access To</h1>
-        <div className="Graphic"><img style ={{width: 815, height: 505}} img src={Graphic} alt="Graphic"/></div>
+        <div className="Graphic"><img src={Graphic} alt="Graphic"/></div>
         <h2>7 days user goal <br/> completion progress</h2>
         <div className="Button4">
           <Button
@@ -174,6 +164,7 @@ export default function CommunityComponent() {
                 fontStyle: "normal",
                 width: 380,
                 height: 90,
+                marginLeft: 100
               }}
               
               variant="contained"
@@ -185,7 +176,6 @@ export default function CommunityComponent() {
               Cycling
             </Button>
         </div>
-        
       </div> 
     </div>
   );
