7e83407e49f9af7453e8dfb0296898385fb602fc
NStav <101630268+NStav@users.noreply.github.com>
2023-09-14 14:50:49 +1000

    Add files via upload

--- src/app/components/sunCycle/SunCycleComponent.jsx
+++ src/app/components/sunCycle/SunCycleComponent.jsx
@@ -5,8 +5,8 @@ import  "./SunCycleStyles.css"
 import LinkedinLogo from "./img/LinkedinLogo.png"
 import instagramLogo from "./img/instagramLogo.png"
 import twitterLogo from "./img/twitterLogo.png"
-
-
+import VRcycle from './img/VRcycle.jpg'
+import VRheadset from './img/VRheadest.jpg'
 export default function AnalyticsComponent() {
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -20,7 +20,29 @@ export default function AnalyticsComponent() {
   return (
     <div>
       <div className="SunCycleBackground">
-      <h1>PROJECT SUN CYCLE</h1>
+        <h1>PROJECT SUN CYCLE</h1>
+        <div className="SunCycleOverview"> 
+          <h1>Overview</h1>
+          <p>
+            Project Sun Cycle is a Virtual Reality based game which is designed to be plated in tandem with one of our Smart Bikes. It involves a single player experience where you 
+            are able to explore and travel across a beautiful post-apocalyptic world completing fun and interesting tasks by traveling throughout this world.
+            <img src={VRcycle} alt="VRcycle"/>
+          </p>
+        </div>
+        <div className="SunCyclePurpose">
+          <h1>PURPOSE</h1>
+          <p> This project strives towards an overall goal of creating a fun and immersive experience that will encourage people to exercise by gamifying exercise and providing a 
+            convenient way to enjoy exercise in the comfort of your own home.
+          </p>
+        </div>
+        <div className="SunCycleHowItWorks">
+          <h1>HOW IT WORKS</h1>
+          <p>
+            <img src={VRheadset} alt="VRheadset"/>
+            Project Sun Cycle will be able to be played using our Smart Bike by simply logging in to your account set up with your Smart Bike Application and Sun Cycle is run from
+            your VR headset you will have the option to pair your devices together and seamlessly enjoy Sun Cycle using your Smart Bike.
+          </p>
+        </div>
       </div>
       <div className="Footer">
 				<div className="FooterContent">
